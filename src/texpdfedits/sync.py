c8888ec755cc3ede3591c6f416ef3e52e06ebc08
Charles Kolozsvary <charleskolozsvary@gmail.com>
2026-06-08 22:45:10 -0400

    synctex.py: finished implementing _improve_synctex_line---appears to work as intended from reviewing realworld test cases. Need to remove debugging ic statements added arxiv05/review.md, flags unexpected lack of autocorrect

--- src/texpdfedits/sync.py
+++ src/texpdfedits/sync.py
@@ -2,23 +2,36 @@
 from pathlib import Path
 import subprocess
 import re
+from icecream import ic
+
+OPT = ('[', ']')
+REQ = ('{', '}')
 
 MACROS_IMPROVE_START = {
-    'caption' : '[{',
-    'bibitem' : '[{',
-    'bib'     : '{{{',
-    'footnote': '[{',
+    'chapter'        : (OPT, REQ),    
+    'section'        : (OPT, REQ),
+    'subsection'     : (OPT, REQ),
+    'subsubsection'  : (OPT, REQ),
+    'chapter*'       : (OPT, REQ),        
+    'section*'       : (OPT, REQ),
+    'subsection*'    : (OPT, REQ),
+    'subsubsection*' : (OPT, REQ),
+    'caption'        : (OPT, REQ),
+    'bib'            : (REQ, REQ, REQ),
+    'footnote'       : (OPT, REQ),
+    'caption*'       : (OPT, REQ),    
 }
 
 ENVS_IMPROVE_START = (
     'equation',
     'align',
     'multline',
+    'gather',
 )
 
 OTHER_IMPROVE_START = {
-    r'\\\[': r'\\\]',
-    r'\$\$': r'\$\$',
+    r'\[': r'\]',
+    r'\bibitem': '\n\n', # I want the entire bibitem until blank line
 }
 
 NUM_LINES_LOOK_BEHIND = 20
@@ -26,6 +39,11 @@ NUM_LINES_LOOK_AHEAD = 30
 
 def _read_balanced(string: str, delimiters: tuple[str, str]) -> tuple[int, int] | None:
     start_delim, end_delim = delimiters
+    if start_delim == end_delim or start_delim.startswith(end_delim):
+        raise ValueError(
+            f"either '{start_delim}' == {end_delim} "
+            f"or '{start_delim}'.startswith('{end_delim}')"
+        )
     encountered_open = False
     idx, stack, start_balanced = 0, 0, 0
     while idx < len(string):
@@ -39,6 +57,9 @@ def _read_balanced(string: str, delimiters: tuple[str, str]) -> tuple[int, int] 
                 stack += 1
                 break
             idx += 1
+            if idx >= len(string):
+                ic(f'string ended while searching for {start_delim}')
+                return None
             char = string[idx]
             substr += char
         while end_delim.startswith(substr) and idx < len(string):
@@ -46,6 +67,9 @@ def _read_balanced(string: str, delimiters: tuple[str, str]) -> tuple[int, int] 
                 stack -= 1
                 break
             idx += 1
+            if idx >= len(string):
+                ic(f'string ended while searching for {end_delim}')
+                return None            
             char = string[idx]
             substr += char
         idx += 1
@@ -53,94 +77,225 @@ def _read_balanced(string: str, delimiters: tuple[str, str]) -> tuple[int, int] 
             return (start_balanced, idx)
     return None
 
+def _first_line_after_char(char_idx: int | None, line2pos: dict[int, int]) -> int | None:
+    if char_idx is None:
+        return None
+    lines_after_char = sorted(
+        line for line, line_start in line2pos.items()
+        if line_start >= char_idx
+    )
+    if not lines_after_char:
+        return None
+    return lines_after_char[0]
+
+def _first_line_before_char(char_idx: int | None, line2pos: dict[int, int]) -> int | None:
+    if char_idx is None:
+        return None
+    lines_before_char = sorted(
+        line for line, line_start in line2pos.items()
+        if line_start <= char_idx
+    )
+    if not lines_before_char:
+        return None
+    return lines_before_char[-1]
+
 def _macro_end_sync(
         ahead_str: str,
         tex_read_start: int,
         synctex_line_start: int,
         macro_name: str,
 ) -> int:
-    raise NotImplementedError()
+    arg_signature = MACROS_IMPROVE_START[macro_name]
+    read_str = ahead_str
+    macro_arg_read_end = tex_read_start # point read up to by macro args
+    for arg in arg_signature:
+        # ic(macro_arg_read_end)
+        arg_span = _read_balanced(read_str, arg)
+        if arg_span is None and arg == REQ:
+            return None
+        if arg_span is None and arg == OPT:
+            continue
+        
+        to_arg_start = read_str[:arg_span[0]]
+        if re.match(r'^[ \t\r]*$', to_arg_start) is None:
+            # expect argument like \caption<single line whitespace><arg>
+            if arg == REQ:
+                # ic(to_arg_start, arg)
+                return None
+            if arg == OPT:
+                # ic(to_arg_start, arg)                
+                continue
+
+        start, end = arg_span
+        macro_arg_read_end += end
+        read_str = read_str[end:]
+    if macro_arg_read_end >= synctex_line_start:
+        return macro_arg_read_end
+    else:
+        # ic(macro_arg_read_end >= synctex_line_start)
+        # if the macro args don't actually contain
+        # (read up to) the original synctex line
+        return None
 
 def _env_end_sync(
         ahead_str: str,
         tex_read_start: int,
         synctex_line_start: int,
         env_name: str,
+        delimiters: tuple[str, str],
 ) -> int:
-    raise NotImplementedError()
+    env_span = _read_balanced(ahead_str, delimiters)
+    if env_span is None:
+        ic('_read_balanced returned None')
+        return None
+    _, env_read_end = env_span
+    tex_read_end = tex_read_start + env_read_end # point read up to by environment 
+    if tex_read_end >= synctex_line_start:
+        return tex_read_end
+    else:
+        ic(tex_read_end >= synctex_line_start)
+        return None
 
-def _other_end_sync(
-        ahead_str: str,
-        tex_read_start: int,
-        synctex_line_start: int,
-        other_name: str,
-) -> int:
-    raise NotImplementedError()
-
-def _end_synctex_span(
+def _end_synctex_line(
         tex_str: str,
         last_match: re.Match,
         match_snippet_start: int,
         line: int,
         line2pos: dict[int, int],
+        improved_start_line: int,
 ) -> int:
-    try:
-        match_type = ('macro', last_match.span('macro'))
-    except IndexError as e:
-        try: 
-            match_type = ('env', last_match.span('env'))
-        except IndexError as e:
-            match_type = ('other', last_match.span('other'))
-    group_id, group_span = match_type
-    
-    read_start = group_span[1] + match_snippet_start
+    ids = ['macro', 'env', 'other']
+    for gid in ids:
+        if last_match.group(gid) is not None:
+            group_id = gid
+            break
+    if group_id == 'macro':
+        # read past \csname to read args
+        tex_read_start = match_snippet_start + last_match.span()[1]
+    else:
+        tex_read_start = match_snippet_start + last_match.span()[0]
     
     end_line = line + NUM_LINES_LOOK_AHEAD
     if end_line not in line2pos:
         end_line = max(line2pos)
         
-    ahead_str = tex_str[read_start:line2pos[end_line]]
-    args = ahead_str, read_start, line2pos[line], last_match.group(group_id)
+    ahead_str = tex_str[tex_read_start:line2pos[end_line]]
+    # ic(ahead_str)
+    args = (
+        ahead_str,
+        tex_read_start,
+        line2pos[line],
+        last_match.group(group_id)
+    )
     if group_id == 'macro':
-        return _macro_end_sync(*args)
+        improved_end = _macro_end_sync(*args)
     elif group_id == 'env':
-        return _env_end_sync(*args)
-    else:
-        return _other_end_sync(*args)
-    
+        env_name = last_match.group(group_id)
+        ic(env_name)
+        delimiters = (rf'\begin{{{env_name}}}', rf'\end{{{env_name}}}')
+        improved_end = _env_end_sync(*args, delimiters)
+    elif group_id == 'other': # other env, really
+        s_delim = last_match.group(group_id)
+        delimiters = (s_delim, OTHER_IMPROVE_START[s_delim])
+        # ic(delimiters)
+        improved_end = _env_end_sync(*args, delimiters)
+        
+    if improved_end is None:
+        ic('improved_end is None')
+        return None
 
-def _improve_synctex_span(
+    improved_end_line = _first_line_after_char(improved_end, line2pos)
+    # ic(improved_end)
+    if group_id == 'other' and s_delim == r'\bibitem':
+        better_end_lines = [
+            line for line in [improved_end_line, improved_end_line - 1]
+            if line > improved_start_line            
+        ]
+        if better_end_lines:
+            improved_end_line = min(better_end_lines)
+            
+    return improved_end_line
+        
+def _improve_synctex_line(
         tex_str: str,
         line: int,
         line2pos: dict[int, int]
 ) -> tuple[int, int]:
     if line + 2 not in line2pos:
         line -= 2        
-    start, end = line2pos[line - NUM_LINES_LOOK_BEHIND], line2pos[line + 1]
-    behind_str = tex_str[start:end]
-    envs = [
-        rf'\\begin\s*{{(?P<env>{env}\*?)}}'
-        for env in ENVS_IMPROVE_START
-    ]
-    macros = [
-        rf'\\(?P<macro>{macro})\b'
-        for macro in MACROS_IMPROVE_START
-    ]
-    other = [
-        rf'(?P<other>{other})'
-        for other in OTHER_IMPROVE_START
-    ]
-    regex = '|'.join(envs + macros + other)
+    line_start, line_end = line2pos[line - NUM_LINES_LOOK_BEHIND], line2pos[line + 1]
+    behind_str = tex_str[line_start:line_end]
     
-    matches = list(re.finditer(regex, behind_str))
-    if not matches:
-        return line2pos[line - 1], line2pos[line + 2]
-    last_match = matches[-1]
-    synctex_span = (
-        start + last_match.span()[0],
-        _end_synctex_span(tex_str, last_match, start)
+    envs = '|'.join(
+        rf'{env}\*?'
+        for env in ENVS_IMPROVE_START
     )
-    return synctex_span
+    envs = rf'\\begin\s*{{(?P<env>{envs})}}'
+    
+    macros = '|'.join(
+        csname for csname in MACROS_IMPROVE_START
+    )
+    macros = rf'\\(?P<macro>{macros})\b'
+
+    others = '|'.join(
+        # don't want to match \\[3em] in display math        
+        r'(?<!\\)\\\[' if other == r'\[' else re.escape(other)   
+        for other in OTHER_IMPROVE_START
+    )
+    others = rf'(?P<other>{others})'
+    
+    regex = '|'.join([envs, macros, others])    
+    matches = list(re.finditer(regex, behind_str))
+
+    # synctex is almost always correct when it is just tracing to
+    # a simple word in the body of the document. It's not
+    # worth extending the lines all the time because then more snippets
+    # will merge than necessary. The whole point of _improve_synctex_line
+    # is to extend the lines when it is appropriate
+    default_start_line = line # line before line - 1
+    default_end_line   = line + 1 # line after line + 2
+
+    default_start_char = line2pos[default_start_line]
+    default_end_char   = line2pos[default_end_line]    
+
+    print('----------------------------------\n\n')
+    ic(line, line2pos[line])
+    # ic(tex_str[default_start_char:default_end_char])
+    # ic(matches)
+    
+    if not matches:
+        return default_start_line, default_end_line
+
+    last_match = matches[-1]
+
+    ic(last_match)
+    improved_start = line_start + last_match.span()[0]
+    improved_start_line = _first_line_before_char(improved_start, line2pos)    
+    improved_end_line = _end_synctex_line(
+        tex_str,
+        last_match,
+        line_start,
+        line,
+        line2pos,
+        improved_start_line,
+    )
+
+    # ic(improved_end_line)
+    
+    if improved_end_line is None:
+        return default_start_line, default_end_line
+
+    # improved_end = line2pos[improved_line_end]
+    # improved_start = line_start + last_match.span()[0]
+    # improved_start_line = _first_line_before_char(improved_start, line2pos)
+    # ic(improved_start_line)
+    
+    if improved_start_line >= improved_end_line:
+        raise RuntimeError(f"'improved' start and ends invalid: {improved_start_line} > {improved_end_line}")
+
+    ic(tex_str[line2pos[improved_start_line]:line2pos[improved_end_line]])
+    
+    return improved_start_line, improved_end_line
 
 def _run_synctex(
         pageno: int,
@@ -311,7 +466,9 @@ def rectangle_to_latex(
     if line_no not in line2pos:
         raise ValueError(f"Line number {line_no} out of range for {input_file}")
 
-    snippet_start, snippet_end = _improve_synctex_span(line_no, line2pos, tex_str)
+    line_start, line_end = _improve_synctex_line(tex_str, line_no, line2pos)
+
+    snippet_start, snippet_end = line2pos[line_start], line2pos[line_end]
     
     latex_snippet = tex_str[snippet_start:snippet_end]
     
---
+++ tests/regression/real_world/arxiv05/review.md
@@ -0,0 +1,1 @@
+Not sure why there was no autocorrect for correction 263
