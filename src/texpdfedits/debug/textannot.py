f7d6f639610a06d03c85ad8ab7cc46a42b2903fe
Charles Kolozsvary <charleskolozsvary@gmail.com>
2026-07-13 15:01:56 -0400

    debug/textannot.py takes pdf_file as command line arg
    Added debug-textannot to pyproject.toml

--- src/texpdfedits/debug/textannot.py
+++ src/texpdfedits/debug/textannot.py
@@ -2,8 +2,29 @@ import pymupdf
 import argparse
 from pathlib import Path
 
-pdf_file = 'statuses_gsm.pdf'
-doc = pymupdf.open(pdf_file)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('pdf_file')
+    args = parser.parse_args()
+
+    pdf_file = Path(args.pdf_file)
+    global doc
+    doc = pymupdf.open(pdf_file)
+
+    while True:
+        command = input()
+        if command.startswith('list'):
+            parts = command.split(' ')
+            if len(parts) > 1:
+                pageno = int(parts[-1])
+            else:
+                pageno = 0
+            list_xrefs(pageno)
+        elif command.startswith('pr'):
+            xref = command.split(' ')[-1]
+            print_xref(int(xref))
+        elif command == 'q':
+            quit()
 
 def list_xrefs(pageno: int=0):
     global doc
